4a3f440d88dbe8f2781524ce99a57ca95330955c
oscar <omatalla@LAPTOP-ULR86CR3.localdomain>
2026-07-22 17:08:42 +0200

    second version from scratch

--- ft_split_Version_2.c
+++ ft_split_Version_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split_Version_2.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omatalla <omatalla@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: omatalla <omatalla@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 17:10:33 by omatalla          #+#    #+#             */
-/*   Updated: 2026/07/21 18:43:38 by omatalla         ###   ########.fr       */
+/*   Updated: 2026/07/22 15:56:44 by omatalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int	word_l(char *s)
 char	*duplicate(char *s)
 {
 	char	*word;
-	int	length;
-	int	i;
+	int		length;
+	int		i;
 
 	length = word_l(s);
 	word = malloc((sizeof(char)) * (length + 1));
@@ -45,4 +45,54 @@ char	*duplicate(char *s)
 	return (word);
 }
 
+void	fill(char **array, char *s)
+{
+	int	i;
 
+	while (*s == ' ' || *s == '\t' || *s == '\n')
+		s++;
+	i = 0;
+	while (*s)
+	{
+		array[i] = duplicate(s);
+		if (!array[i])
+			return ;
+		i++;
+		while (*s && *s != ' ' && *s != '\t' && *s != '\n')
+			s++;
+		while (*s == ' ' || *s == '\t' || *s == '\n')
+			s++;
+	}
+}
+
+int	count_w(char *s)
+{
+	int	i;
+
+	while (*s == ' ' || *s == '\t' || *s == '\n')
+		s++;
+	i = 0;
+	while (*s)
+	{
+		i++;
+		while (*s && *s != ' ' && *s != '\t' && *s != '\n')
+			s++;
+		while (*s == ' ' || *s == '\t' || *s == '\n')
+			s++;
+	}
+	return (i);
+}
+
+char	**ft_split(char *s)
+{
+	int		word_n;
+	char	**array;
+
+	word_n = count_w(s);
+	array = malloc(sizeof(char *) * (word_n + 1));
+	if (!array)
+		return (NULL);
+	array[word_n] = NULL;
+	fill(array, s);
+	return (array);
+}
