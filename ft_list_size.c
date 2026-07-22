2c0708331ce0ba4c019fb1c8a1b03013e10be2e8
oscar <omatalla@LAPTOP-ULR86CR3.localdomain>
2026-07-22 17:16:26 +0200

    ft_list_size.c plus subject

---
+++ ft_list_size.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omatalla <omatalla@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/22 17:14:58 by omatalla          #+#    #+#             */
+/*   Updated: 2026/07/22 17:15:02 by omatalla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	count;
+
+	count = 0;
+	while (begin_list)
+	{
+		count++;
+		begin_list = begin_list->next;
+	}
+	return (count);
+}
